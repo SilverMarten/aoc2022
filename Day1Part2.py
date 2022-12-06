ceec5a8c8eb6c82d6ef5faede07eabe388a4c9e7
Paul Cormier <lastloginname@gmail.com>
2022-12-05 22:34:02 -0500

    Simplify Day 1 - part 2

--- Day1Part2.py
+++ Day1Part2.py
@@ -1,17 +1,13 @@
 
-calories = {1:0}
-elf = 1
+calories = [0]
 
 with open('inputs/Day1.txt') as input:
     for line in input.readlines():
-        # print(line)
         if line == '\n':
-            elf += 1
-            calories[elf] = 0
+            calories.append(0)
         else:
-            calories[elf] += int(line)
+            calories[-1] += int(line)
 
-# top3Calories = sorted(calories, key=calories.get, reverse=True)[:3]
-top3Calories = dict(sorted(calories.items(), key=lambda item: item[1], reverse=True)[:3])
+top3Calories = sorted(calories, reverse=True)[:3]
 
-print(f'The top three most calories is #{top3Calories} with a sum of {sum(top3Calories.values())} calories.')
+print(f'The top three most calories is #{top3Calories} with a sum of {sum(top3Calories)} calories.')
