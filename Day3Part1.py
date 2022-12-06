f8dd550f2bdad2bec0d6d1bcf19af65473a464b5
Paul Cormier <lastloginname@gmail.com>
2022-12-06 10:29:58 -0500

    Solve Day 3 - part 1

---
+++ Day3Part1.py
@@ -0,0 +1,18 @@
+prioritySum = 0
+
+compartments = ['','']
+
+with open('inputs/Day3.txt') as input:
+    for line in input.readlines():
+        line = line.strip('\n\r')
+        compartments[0] = line[:int(len(line)/2)]
+        compartments[1] = line[int(len(line)/2):]
+        # print(compartments)
+        # https://www.geeksforgeeks.org/python-intersection-two-lists/
+        intersection = list(set(compartments[0]) & set(compartments[1]))[0]
+        # print(intersection)
+        priority = ord(intersection) - (ord('a') - 1 if intersection.islower() else ord('A') - 27)
+        # print(priority)
+        prioritySum += priority
+
+print(f'The sum of the item priorities is {prioritySum}.')
