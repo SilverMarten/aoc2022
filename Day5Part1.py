eae8b588843469a58d4c9603dec64affa300293a
Paul Cormier <lastloginname@gmail.com>
2022-12-06 19:34:15 -0500

    Solve Day 5 - part 1

--- Day5Part1.py
+++ Day5Part1.py
@@ -1,14 +1,33 @@
 '''After the rearrangement procedure completes, what crate ends up on top of each stack?'''
 
-stacks = []
+stacks = [[] for i in range(9)]
 
-with open('inputs/Day5 sample.txt') as input:
+with open('inputs/Day5.txt') as input:
+    lines = input.read().splitlines()
+
     # Read the starting conditions
-    for line in input.read().splitlines():
-        if line == '': break
+    for line in lines:
+        # print(line)
+        if line == '' or line.startswith(' 1'): break
+
+        for i in range(int((len(line)+1)/4)):
+            box = line[i * 4 + 1]
+            if box.isalpha(): stacks[i].append(box)
+
+    for stack in stacks: stack.reverse() 
+    # print(stacks)
 
     # Read the instructions and process them
-    for line in input.read().splitlines():
-        fromStack = line.split(' from' )[0]
+    for line in lines:
+        if not line.startswith('move'): continue
 
-print(f'After the rearrangement procedure, the top of the stacks are: {[box[0] for box in stacks]}')
+        toStack = int(line[-1])
+        fromStack = int(line[-6])
+        howMany = int(line[5:-12])
+        # print(f'move {howMany} from {fromStack} to {toStack}')
+        for i in range(howMany):
+            stacks[toStack - 1].append(stacks[fromStack - 1].pop())
+        
+        # print(stacks)
+
+print(f'After the rearrangement procedure, the top of the stacks are: {[box[-1] for box in stacks if len(box) > 0]}')
