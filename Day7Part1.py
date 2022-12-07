f272f3ea2949221a0075411e80e409afe13686cd
Paul Cormier <lastloginname@gmail.com>
2022-12-07 15:35:32 -0500

    Solve for Day 7 - part 1

--- Day7Part1.py
+++ Day7Part1.py
@@ -1,25 +1,81 @@
+import sys
+
 '''
 Find all of the directories with a total size of at most 100000.
 What is the sum of the total sizes of those directories?
 '''
 
-fileSystem = {}
+class Node:
+    '''A node in a file system'''
+    def __init__(self, name, type, parent = None, bytes = 0):
+        self.name = name
+        self.type = type
+        self.contents = {}
+        self.parent = parent
+        self.bytes = int(bytes)
+        
+    def __str__(self):
+        return f'{self.bytes} {self.name}' if self.type == 'file' else \
+               f'{self.type} {self.name}\n' + \
+               '\n'.join([str(dir) for dir in self.contents.values()])
 
-pwd = []
+    def size(self):
+        '''Compute the size of the node based on its type'''
+        if self.type == 'file': return self.bytes
 
-with open('inputs/Day7 sample.txt') as input:
+        return sum([dir.size() for dir in self.contents.values()])
+
+root = Node('/', 'dir')
+
+pwd = root
+
+with open('inputs/Day7.txt') as input:
     lines = input.read().splitlines()
 
-    for i in len(lines):
+    i = 0
+    while i < len(lines):
         line = lines[i]
         # print(line)
         
-        # Go back up one in the path
-        if line.startswith('$ cd ..'):
-            pwd.pop()
+        # Handle the commands
+        if line.startswith('$ cd /'):
+            # print('Switching to root.')
+            pwd = root
+        elif line.startswith('$ cd ..'):
+            # print('Switching to parent.')
+            pwd = pwd.parent
         elif line.startswith('$ cd'):
-            dir = line[6:]
+            dir = line[5:]
+            # print(f'Switching to {dir}.')
+            pwd = pwd.contents[dir]
+        elif line.startswith('$ ls'):
+            # Parse each line of the directory listing
+            node = None
+            while i+1 < len(lines) and not lines[i+1].startswith('$'):
+                i += 1
+                line = lines[i].split()
+                name = line[1]
+                if line[0] == 'dir':
+                    node = Node(name, 'dir', pwd)
+                    # print(f'Creating directory {name}')
+                else:
+                    node = Node(name, 'file', pwd, line[0])
+                    # print(f'Creating file {node}')
+                pwd.contents[name] = node
 
-            pwd.append(dir)
+        i += 1
 
-print(f'The first start-of-packet marker is detected after {i+1} characters.')
+# print(root)
+
+total = 0
+# Work out the size of each directory
+directoriesToCheck = [root]
+while len(directoriesToCheck) > 0:
+    dir = directoriesToCheck.pop(0)
+    dirSize = dir.size()
+    if dirSize <= 100_000:
+        total += dirSize
+    
+    directoriesToCheck.extend([dir for dir in dir.contents.values() if dir.type == 'dir'])
+
+print(f'The sum of the directories with a size of 100000 or less is {total}.')
