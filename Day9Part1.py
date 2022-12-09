aad4f24ab6b250481fcf28726d1694e9afbdc67b
Paul Cormier <lastloginname@gmail.com>
2022-12-09 07:33:28 -0500

    Start Day 9 - part 1

---
+++ Day9Part1.py
@@ -0,0 +1,43 @@
+import logging
+
+'''How many positions does the tail of the rope visit at least once?'''
+
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+with open('inputs/Day9 sample.txt') as input:
+    lines = input.read().splitlines()
+
+headPosition = (0,0)
+tailPosition = (0,0)
+visitedPositions = set()
+
+for line in lines:
+    logging.debug(line)
+    for move in range(int(line[2])):
+        # Move head
+        match line[0]:
+            case 'U':
+                headPosition = (headPosition[0], headPosition[1] + 1)
+            case 'D':
+                headPosition = (headPosition[0], headPosition[1] - 1)
+            case 'R':
+                headPosition = (headPosition[0] + 1, headPosition[1])
+            case 'L':
+                headPosition = (headPosition[0] - 1, headPosition[1])
+
+        logging.debug(f'Head: {headPosition}')
+        # Move tail
+        deltaX = headPosition[0]-tailPosition[0]
+        deltaY = headPosition[1]-tailPosition[1]
+
+        if deltaX > 1 or deltaX < 1:
+            tailPosition = (tailPosition[0] + deltaX, tailPosition[1])
+        
+        if deltaY > 1 or deltaY < 1:
+            tailPosition = (tailPosition[0], tailPosition[1] + deltaY)
+        
+        visitedPositions.add(tailPosition)
+        logging.debug(f'Tail: {tailPosition}')
+            
+            
+print(f'The tail of the rope visits {len(visitedPositions)} positions.')
