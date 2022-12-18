02d5d27824b428a753b990856b53ae4db13e6c09
Paul Cormier <lastloginname@gmail.com>
2022-12-18 10:51:40 -0500

    Solve Day 18 - part 2

--- Day18Part2.py
+++ Day18Part2.py
@@ -1,24 +1,11 @@
 import logging, Colorer
 from tqdm import tqdm
 from collections import deque
+from Tools import between
 
 '''
 What is the surface area of your scanned lava droplet (without pockets)?
 '''
-
-def isPocket(coordinates, droplets):
-    '''There a contained pocket here.'''
-    x,y,z = coordinates
-    dropletCount = 0
-    for delta in [-1,1]:
-        if (x+delta, y, z) in droplets: 
-            dropletCount += 1
-        if (x, y+delta, z) in droplets: 
-            dropletCount += 1
-        if (x, y, z+delta) in droplets: 
-            dropletCount += 1
-
-    return dropletCount == 6
 
 logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 log = logging.getLogger()
@@ -26,33 +13,59 @@ log = logging.getLogger()
 with open('inputs/Day18.txt') as inputFile:
     lines = inputFile.read().splitlines()
 
-droplets = deque()
+droplets = set()
 for line in lines:
-    droplets.append(eval(f'({line})'))
+    droplets.add(eval(f'({line})'))
 
-allDroplets = set(droplets)
-totalDroplets = len(droplets)
+# Eureka!
+# Create a volume of steam around the chunk of lava and calculate its surface area
+
+minCoordinate = min([min(x,y,z) for x,y,z in droplets]) - 1
+maxCoordinate = max([max(x,y,z) for x,y,z in droplets]) + 1
+
+log.debug(f'Checking from {minCoordinate} to {maxCoordinate}.')
+
+# Figure out the locations of the steam droplets
+start = (minCoordinate,minCoordinate,minCoordinate)
+steam = set()
+steam.add(start)
+toCheck = deque()
+toCheck.append(start)
+
+while toCheck:
+    x,y,z = toCheck.pop()
+    
+    # Check neighbours, add steam to queue and set
+    for delta in [-1,1]:
+        neighbour = (x+delta, y, z)
+        if between(x+delta, minCoordinate, maxCoordinate) and\
+             neighbour not in droplets and neighbour not in steam: 
+            steam.add(neighbour)
+            toCheck.append(neighbour)
+        neighbour = (x, y+delta, z)
+        if between(y+delta, minCoordinate, maxCoordinate) and\
+             neighbour not in droplets and neighbour not in steam: 
+            steam.add(neighbour)
+            toCheck.append(neighbour)
+        neighbour = (x, y, z+delta)
+        if between(z+delta, minCoordinate, maxCoordinate) and\
+             neighbour not in droplets and neighbour not in steam: 
+            steam.add(neighbour)
+            toCheck.append(neighbour)
+
+# Find the surface area of the steam
 contiguousFaces = 0
-pockets = set()
-
-while droplets:
-    x,y,z = droplets.pop()
+steamDroplets = deque(steam)
+while steamDroplets:
+    x,y,z = steamDroplets.pop()
     # Check neighbours, count contiguous faces
     for delta in [-1,1]:
-        if (x+delta, y, z) in droplets: 
+        if (x+delta, y, z) in steamDroplets: 
             contiguousFaces += 1
-        elif isPocket((x+delta, y, z), allDroplets):
-            pockets.add((x+delta, y, z))
-        if (x, y+delta, z) in droplets: 
+        if (x, y+delta, z) in steamDroplets: 
             contiguousFaces += 1
-        elif isPocket((x, y+delta, z), allDroplets):
-            pockets.add((x, y+delta, z))
-        if (x, y, z+delta) in droplets: 
+        if (x, y, z+delta) in steamDroplets: 
             contiguousFaces += 1
-        elif isPocket((x, y, z+delta), allDroplets):
-            pockets.add((x, y, z+delta))
 
-log.debug(f'{len(pockets)} at: {sorted(pockets)}')
-
-area = 6 * totalDroplets - 2 * contiguousFaces - 6 * len(pockets)
+area = 6 * len(steam) - 2 * contiguousFaces - 6 * (maxCoordinate - minCoordinate + 1) ** 2
 print(f'The surface area of the lava droplet is: {area}')
